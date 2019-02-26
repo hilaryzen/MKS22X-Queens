3d27ab154d02a1217887a4b8a64f4809350970ff
Hilary Zen <hzen00@stuy.edu>
2019-02-26 00:14:42 -0500

    Added driver

--- Driver.java
+++ Driver.java
@@ -33,7 +33,7 @@ public class Driver {
 
     System.out.println(" ");
 
-    QueenBoard board2 = new QueenBoard(14);
+    QueenBoard board2 = new QueenBoard(30);
     System.out.println(board2.solve());
     System.out.println(board2);
 
@@ -47,8 +47,8 @@ public class Driver {
 
     QueenBoard board4 = new QueenBoard(4);
     System.out.println("Number of solutions for size 4: " + board4.countSolutions());
-    QueenBoard board5 = new QueenBoard(8);
-    System.out.println("Number of solutions for size 8: " + board5.countSolutions());
+    QueenBoard board5 = new QueenBoard(14);
+    System.out.println("Number of solutions for size 14: " + board5.countSolutions());
 
     System.out.println(" ");
 
